23e76ec203038cd18e5499182439e3250d129cd7
fedebarrazaa <fedebarraza97@gmail.com>
2026-05-27 18:51:36 -0300

    new desing page

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,22 +1,52 @@
-import style from './Login.module.css'
+import { useState } from 'react';
+import style from './Login.module.css' //import css
+import { Link, useNavigate } from 'react-router-dom'; //para el router
+import { supabase } from '../../../lib/supabase';
+
+
+
 
 export function DesingLoginPage(){
+    //GUARDO LOS DATOS QUE PONEN EN INPUT CON useState y onChange: line 27 y 32
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+
+    const navigate = useNavigate(); //queda vacio
+    //
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>  {//conceccion de base de datos hasta la linea 29
+            e.preventDefault(); //
+    
+            const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
+    
+        if (error) {
+            console.log("Error:", error.message)
+        } else {
+            navigate('/dashboard') //reedirigue a la pagina del dashboard
+        }
+    
+        }
+
     return(
         <section className={style.desing_section}>
-        
          <h1 className={style.desing_h1}>Organiza, controla y mira tus tareas desde cualquier lugar</h1>
-        
-         <form className={style.desing_form}>
+         <form className={style.desing_form} onSubmit={handleSubmit}>
             <div> 
                 <p className={style.div_p}>〰️</p>
-                <h1 className={style.div_h1}> Inicia seccion </h1>
+                <h1 className={style.div_h1}> Iniciar seccion </h1>
                 <h3 className={style.div_h3}>Acceda a sus tareas, notas y proyectos en cualquier momento.</h3>
             </div>
             <input 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             type="Email"
             placeholder='Email'
             className={style.desing_email}></input>
             <input 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password" 
             placeholder='Contraseña'
             className={style.desing_pass}></input>
@@ -25,11 +55,8 @@ export function DesingLoginPage(){
             value="Ingresa"
             className={style.desing_boton}
             ></input> 
-            
-            
-             
+            <Link to="/register" className={style.desing_link}>Crear cuenta</Link>
          </form>
         </section>
-        
     )
 }
--- src/pages/auth/register/RegisterPage.tsx
+++ src/pages/auth/register/RegisterPage.tsx
@@ -1,9 +1,43 @@
-
+import { useState } from 'react';
+import style from './Register.module.css' //import css
+import { Link, useNavigate } from 'react-router-dom'; //para el router
+import { supabase } from '../../../lib/supabase';
 
 export function DesingRegisterPage(){
     return (
-        <h1> 
-            
-        </h1>
+        <section className={style.desing_section}>
+         {/*<h1 className={style.desing_h1}>Organiza, controla y mira tus tareas desde cualquier lugar</h1>*/}
+         <form className={style.desing_form} >
+            <div> 
+                <p className={style.div_p}>〰️</p>
+                <h1 className={style.div_h1}> Crear cuenta </h1>
+                <h3 className={style.div_h3}>Crea tu cuenta en simple pasos y empieza a organizar tus tareas.</h3>
+            </div>
+            <input
+            type="text"
+            placeholder='Nombre'
+            className={style.desing_nombre}
+            ></input>
+            <input
+            type="text"
+            placeholder='Apellido'
+            className={style.desing_apellido}
+            ></input>
+            <input 
+            type="Email"
+            placeholder='Email'
+            className={style.desing_email}></input>
+            <input 
+            type="password" 
+            placeholder='Contraseña'
+            className={style.desing_pass}></input>
+            <input 
+            type="submit"
+            value="Ingresa"
+            className={style.desing_boton}
+            ></input> 
+            <Link to="/register" className={style.desing_link}>Crear cuenta</Link>
+         </form>
+        </section>
     )
 }
